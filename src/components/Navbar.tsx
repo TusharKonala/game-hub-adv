f99c7597ee1065abe29d8f007e766bafd5908a1c
Tushar Konala <tusharkonala284@gmail.com>
2025-10-18 12:10:52 +0530

    Built a navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const Navbar = () => {
+  return (
+    <>
+      <HStack>
+        <Image src={logo} boxSize="60px" />
+      </HStack>
+    </>
+  );
+};
+
+export default Navbar;
